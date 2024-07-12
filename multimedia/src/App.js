42dcda70bdf8cb1b7b9a7f1e3ff706642910305a
Laura-Nyaaga <nyaagaaluoch@gmail.com>
2024-07-12 14:55:47 +0300

    added the landingpage and navbar

--- multimedia/src/App.js
+++ multimedia/src/App.js
@@ -1,12 +1,18 @@
+
+import React from 'react';
 import './App.css';
 import Footer from './Footer'
+import LandingPage from './LandingPage';
+import NavBar from './NavBar';
+
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      <LandingPage/>
       <Footer/>
-
-
     </div>
   );
 }
